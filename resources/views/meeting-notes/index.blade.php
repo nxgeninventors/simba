d54197eaa11164ebdba7c1f6c927bf7750c3735f
muni <muni2explore@gmail.com>
2023-01-20 10:37:24 +0530

    Create meeting note link added

--- resources/views/meeting-notes/index.blade.php
+++ resources/views/meeting-notes/index.blade.php
@@ -11,7 +11,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100 min-h-screen">
-                    <h2>Meeting Notes</h2>
+                    <div class="flex justify-between">
+                        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                            {{ __('Meeting Notes') }}
+                        </h2>
+
+                        <a href="{{ url('meeting-notes/create') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Create New Note
+                        </a>
+                    </div>
+
                     <table style="width: 100%; overflow: hidden" id="meeting_notes_table">
                     </table>
                 </div>
