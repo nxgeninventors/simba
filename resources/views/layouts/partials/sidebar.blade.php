8a73a1f421fae001b051444a0ec177396274daff
muni <muni2explore@gmail.com>
2023-03-18 10:23:31 +0530

    meeting notes layout changed

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -30,6 +30,12 @@
           </li>
 
           <li>
+            <a href="/meeting-notes" class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
+              <i class="fa-solid fa-clipboard-list w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"></i>  
+              <span class="ml-3" sidebar-toggle-item="">Meeting Notes</span>
+            </a>
+          </li>
+          <li>
             <a href="/projects" class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
               <i class="fa-solid fa-clipboard-list w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"></i>  
               <span class="ml-3" sidebar-toggle-item="">Projects</span>
--- resources/views/meeting-notes/edit.blade.php
+++ resources/views/meeting-notes/edit.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-base-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
@@ -11,7 +11,7 @@
         <form action="{{ url("meeting-notes/".$meetingNote->id) }}" method="POST">
             @csrf
             @method('PUT')
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="px-4 pt-6 mx-auto">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100 min-h-screen">
                         <div class="mb-4">
@@ -36,4 +36,4 @@
             </div>
         </form>
     </div>
-</x-app-layout>
+</x-base-layout>
