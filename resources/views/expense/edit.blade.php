3857d1621b592eff7bb172e1b75f9dc09f4d0e7f
muni <muni2explore@gmail.com>
2023-04-22 12:23:24 +0530

    Fix

--- resources/views/expense/edit.blade.php
+++ resources/views/expense/edit.blade.php
@@ -91,7 +91,7 @@
                                                     </div>
                                                     <div class="">
                                                         <label for="doc_label_1" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Doc Label</label>
-                                                        <input name="expense_docs[0][doc_label]" id="doc_label_1" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Doc Label" required="">
+                                                        <input name="expense_docs[0][doc_label]" id="doc_label_1" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Doc Label">
                                                     </div>
                                                 </div>
                                             </div>
